7201b6192ed904ce8451423463bed55d6dfbc7f8
vaLApollo <73369501+vaLApollo@users.noreply.github.com>
2023-03-21 17:45:36 -0400

    kek

---
+++ login.php
@@ -0,0 +1,31 @@
+<h1>Feizi Lab Q1: Log-in API</h1>
+
+Login Below:
+<br><br>
+
+<form>
+   Username: <input type="text" id="user">
+   <br>
+   Password: <input type="text" id="pass">
+   <br>
+   <button onclick="process_login_info()">Submit</button>
+</form>
+
+<br><br>
+Don't have an account? 
+<div style="color: blue"; onclick="load_signup_page()"><u>Sign up</u></div>
+
+<script>
+   function process_login_info() {
+      var user = document.getElementById("user");
+      var pass = document.getElementById("pass");
+
+      <? ?>
+
+
+   }
+
+   function load_signup_page() {
+
+   }
+</script>
